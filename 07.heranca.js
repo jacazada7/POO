a211be1249dfddd05dd832d61e93861e28835dd2
Jaca01 <joaoalvves028@gmail.com>
2024-09-19 08:26:54 -0400

    classes diretor aluno e professor

--- 07.heranca.js
+++ 07.heranca.js
@@ -43,3 +43,17 @@ class Professor extends Funcionário {
         
     }
 }
+
+// Criando objetos aluno, diretor e professor
+
+const aluno = new Estudante("João", 16, 1.70, 70, "Programação de Jogos Digitais");
+const diretor = new Diretor("Sergio", 45, 1.65, 65, 3000);
+const professor = new Professor("Wanderson", 40, 1.65, 70, 1000);
+
+// Mostrando no console a saída
+
+console.log(aluno); // Saída: Estudante (nome: `João`, idade: 16, altura: 1.7, peso: 70, curso: ´Programação de Jogos Digitais`)
+console.log(diretor); //Saída: Diretor (nome: `Sergio`, idade: 45, altura: 1.65, peso: 65, salário: 3000 }
+console.log(professor); // Saída: Professor (nome: `Wanderson´, idade: 40, altura: 1.65, peso 70, salario 1000)
+
+
