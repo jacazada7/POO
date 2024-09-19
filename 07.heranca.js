4a9d8b8a0053d11534d8e3ad45703eca244b29b1
Jaca01 <joaoalvves028@gmail.com>
2024-09-19 07:48:23 -0400

    modificado heranca

--- 07.heranca.js
+++ 07.heranca.js
@@ -7,3 +7,13 @@ class Pessoa {
         this.peso = peso;
     }
 }
+
+// Classe Estudante que herda de Pessoa
+
+class Estudante extends Pessoa {
+    constructor(nome, idade, altura, peso, curso) {
+        super(nome, idade, altura, peso);
+        this.curso = curso;
+        
+    }
+}
