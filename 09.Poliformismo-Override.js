b7101cd2bb6cc4c29462fa7394c97cd7f162b838
Jaca01 <joaoalvves028@gmail.com>
2024-09-19 10:54:43 -0400

    object

--- 09.Poliformismo-Override.js
+++ 09.Poliformismo-Override.js
@@ -55,3 +55,19 @@ class Arqueiro extends Personagem {
         console.log(` ${this.nome} dispara flechas ${this.arma}!`);
     }
 }
+
+// Criando instâncias de Guerreiro, Mago e Arqueiro
+
+const guerreiro = new Guerreiro("Arthur", 100, "Espada");
+guerreiro.atacar(); // Saída: Arthur ataca com a espada!
+guerreiro.receberDano(20);  // Saída: Arthur recebeu 20 de dano. Vida restante: 80
+
+const mago = new Mago("Loki", 80, "Cura Eterna");
+mago.atacar(); // Saída: Merlin lança bola de fogo!
+mago.receberDano(15);  // Saída: Merlin recebeu 15 de dano. Vida restante: 65
+
+const arqueiro = new Arqueiro("Legolas", 90, "explosiva");
+arqueiro.atacar(); // Saída: Legolas dispara flechas explosiva
+arqueiro.receberDano(10);  // Saída: Legolas recebeu 10 de dano. Vida restante: 80
+
+
