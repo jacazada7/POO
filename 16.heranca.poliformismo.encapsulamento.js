8199dadc1b16d63e96d327dd567021f33824bc04
Jaca01 <joaoalvves028@gmail.com>
2024-10-01 08:19:30 -0400

    add heranca poliformismo encapsulamento

---
+++ 16.heranca.poliformismo.encapsulamento.js
@@ -0,0 +1,31 @@
+class Personagem {
+      //Encapsulando os atributos com #, tornando-os privados.
+  #nome;
+  #classe;
+  #nivel;
+  #vida;
+  #mana;
+  // Método Contrutor
+  constructor(nome, classe, nivel, vida, mana) {
+    this.#nome = nome;
+    this.#classe = classe;
+    this.#nivel = nivel;
+    this.#vida = vida;
+    this.#mana = mana;
+  }
+  // Métodos Getters e Setters
+  //Métodos Getters e Setters para o atributo nome
+  get nome() {
+    return this.#nome;
+  }
+  set nome(nome) {
+    this.#nome = nome;
+  }
+  //Métodos Getters e Setters para o atributo classe
+  get classe() {
+    return this.#classe;
+  }
+  set classe(classe) {
+    this.#classe = classe;
+  }
+}
