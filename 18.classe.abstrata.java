79cf4fb89c6b9560bcf544bd5873d79d5b36a0be
Jaca01 <joaoalvves028@gmail.com>
2024-10-22 09:53:14 -0400

    add clase abstract

---
+++ 18.classe.abstrata.java
@@ -0,0 +1,12 @@
+abstract class Personagem {
+    protected String nome;
+
+
+    public Personagem(String nome) {
+        this.nome = nome;
+    }
+
+    public abstract void atacar();
+    public abstract void defender();
+    
+}
